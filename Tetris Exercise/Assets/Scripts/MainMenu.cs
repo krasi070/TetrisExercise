46521d9d23cadaaebc8330d8192d80d25ddb6e3b
krasi070 <krasi0707@gmail.com>
2017-09-12 02:01:55 +0300

    made play and exit buttons

---
+++ Tetris Exercise/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+		
+	}
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
